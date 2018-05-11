4681cdf31412bd1a13910b763045eac1b9f77156
maxuss777 <borodatogo@gmail.com>
2018-05-11 04:03:36 +0300

    some editing

--- ChromeExtentions/js/popup.js
+++ ChromeExtentions/js/popup.js
@@ -1,7 +1,43 @@
 $("#glyphicon").on("click", changeIcon);
 $("#site-actions").on("click", applyOption);
 
-function changeIcon() {
+var result=[];
+$(document).ready(function () {
+   getAllChromeLocalStorageKeys();
+   alert(result[0]);
+
+    renderKey(result[0]);
+
+    // $().each(keys, function(index, value){
+    //     var items = getItemsFromChromeLocalStorage(value);
+    //     renderKey(value);
+    //     $().each(items, function(index, value){
+    //         renderItem(value);
+    //     });
+    // });
+})
+
+var renderKey = function(key) {
+    $("#accordion > div").append(
+        '<div class="panel-heading" role="tab" id="headingOne"><h4 class="panel-title"> ' + key +
+        ' <a target="_blank" role="button" data-toggle="collapse"' +
+        'data-parent="#accordion" href="#collapseOne" aria-expanded="false"aria-controls="collapseOne" class="collapsed"><span id="glyphicon"' +
+        'class="glyphicon glyphicon-plus"></span></a></h4></div>'
+    );   
+}
+var renderItem = function(item) {
+    $("#accordion > div").append(
+        '<div id="collapseOne" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne" aria-expanded="false"><div class="panel-body">' +
+        '<div class="input-group"><input id="item-path-input" type="text" value="' + item.path + '" class="form-control" placeholder="item path" disabled><input id="attributes-input"' +
+        'type="text" value="' + item.styles + '" class="form-control" placeholder="attributes" disabled><div id="site-actions" class="btn-group" data-toggle="buttons">' +
+        '<label id="doNothingOption" class="btn btn-primary active"><input type="radio" name="options" autocomplete="off"> Do nothing</label>' +
+        '<label  id="deleteOption" class="btn btn-primary"><input type="radio" name="options"autocomplete="off">' +
+        'Delete</label><label id="changeOption" class="btn btn-primary"><input type="radio" name="options" autocomplete="off">' +
+        'Change</label></div></div></div></div>'
+    );
+}
+
+var changeIcon = function () {
     var collapseOne = $("#collapseOne");
     var span = $("#glyphicon");
     if (collapseOne.attr("aria-expanded") === "true") {
@@ -14,7 +50,7 @@ function changeIcon() {
     }
 }
 
-function applyOption(event) {
+var applyOption = function (event) {
     switch (event.target.id) {
         case "deleteOption":
             $("#item-path-input").prop("disabled", false);
@@ -28,4 +64,22 @@ function applyOption(event) {
             $("#attributes-input").prop("disabled", false);
             break;
     }
+}
+
+var getAllChromeLocalStorageKeys = function () {
+    
+    
+    chrome.storage.local.get(null, function (keys) {        
+        $.each(Object.keys(keys),function(index, value){
+            result.push(value);
+        });
+    });
+
+    
+}
+
+var getItemsFromChromeLocalStorage = function (key) {
+    chrome.storage.local.get(key, function (result) {
+        return Object.values(result).toArray();
+    });
 }
